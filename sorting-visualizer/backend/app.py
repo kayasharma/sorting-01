c7de67b941505f9fcd61546bb6775db2aa45aebf
kanchan sharma <166435191+kayasharma@users.noreply.github.com>
2024-12-08 22:13:49 +0530

    Update app.py

--- sorting-visualizer/backend/app.py
+++ sorting-visualizer/backend/app.py
@@ -293,5 +293,5 @@ def counting_sort(arr, algorithm):
     return arr, metrics
 
 # Run the Flask application
-if __name__ == '__main__':
-    socketio.run(app, debug=True)
+if _name_ == '_main_':
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
